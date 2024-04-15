420e869a2b404a6f1785269f025a605f2670c6c0
Tony Cafisso <tonycafisso@gmail.com>
2024-04-14 21:33:08 -0300

    Product

--- src/main/java/com/educandoweb/course/entites/Category.java
+++ src/main/java/com/educandoweb/course/entites/Category.java
@@ -1,9 +1,10 @@
 package com.educandoweb.course.entites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_category")
@@ -15,6 +16,10 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @JsonIgnore
+    @Transient
+    private Set<Category> products = new HashSet<>();
+
 
     public Category(){
 
@@ -41,6 +46,9 @@ public class Category {
         this.name = name;
     }
 
+    public Set<Category> getProducts() {
+        return products;
+    }
 
     @Override
     public boolean equals(Object o) {
